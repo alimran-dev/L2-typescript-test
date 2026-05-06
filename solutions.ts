a1f675358b67b2499fef2649b6300f2934c7ccf8
alimran-dev <alimran5air@gmail.com>
2026-05-06 20:08:43 +0600

    solved problem 4

--- solutions.ts
+++ solutions.ts
@@ -1,7 +1,9 @@
+// problem 1
 const filterEvenNumbers=(nums: number[]):number[]=>{
   return nums.filter((num)=>num%2==0)
 }
 
+// problem 2
 const reverseString=(input: string): string=>{
   let rev = "";
   for(let i=input.length - 1;i>=0;i--){
@@ -10,6 +12,7 @@ const reverseString=(input: string): string=>{
   return rev;
 }
 
+// problem 3
 type StringOrNumber = string | number;
 const checkType=(input: StringOrNumber)=>{
   if(typeof input === "string"){
@@ -19,3 +22,7 @@ const checkType=(input: StringOrNumber)=>{
   }
 }
 
+// problem 4
+const getProperty=<T,K extends keyof T>(obj: T,key: K):T[K]=>{
+  return obj[key];
+}
